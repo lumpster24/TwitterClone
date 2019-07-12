3583219244acfe3c96e3e21b9c93e4fe40a57b21
Ian <ianbingham24@gmail.com>
2019-07-11 19:49:22 -0700

    sign up page logic complete

--- ui/src/pages/Signup.jsx
+++ ui/src/pages/Signup.jsx
@@ -1,44 +1,68 @@
 import React, { useState } from "reactn";
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import axios from 'axios'
 
 const Signup = () => {
-  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [signupSuccess, setSignupSuccess] = useState(false)
+  const [errorMessage, setErrorMessage] = useState(null)
 
-  const onSubmit = (evt) => {
+  const baseUrl = 'http://localhost:4000'
+
+  const onSubmit = async (evt) => {
     evt.preventDefault()
+    setSignupSuccess(false)
 
-    alert('submitted signup!')
+    try {
+      await axios.post(`${baseUrl}/auth/signup`, {
+        email,
+        password,
+        passwordConfirm
+      })
+
+      alert('signup complete!')
+      setErrorMessage(null)
+      setSignupSuccess(true)
+    } catch(err) {
+      setErrorMessage(err.response.data.error)
+    }
   }
 
   return (
     <>
-      <h2>Signup</h2>
-      <form onSubmit={onSubmit}>
-        <input 
-          type='text' 
-          placeholder='Username...'
-          value={username} 
-          onChange={(evt) => setUsername(evt.target.value)} 
-        />
-        <input 
-          type='text' 
-          placeholder='Password...'
-          value={password} 
-          onChange={(evt) => setPassword(evt.target.value)} 
-        />
-        <input 
-          type='text' 
-          placeholder='Password Confirm...'
-          value={passwordConfirm} 
-          onChange={(evt) => setPasswordConfirm(evt.target.value)} 
-        />
-        <input type="submit" value="Submit" />
-      </form>
-      <div>
-        <Link to="/">Back to Login</Link>
-      </div>
+      {!signupSuccess ? 
+        (<>
+            <h2>Signup</h2>
+            <form onSubmit={onSubmit}>
+              <input 
+                type='text' 
+                placeholder='Email...'
+                value={email} 
+                onChange={(evt) => setEmail(evt.target.value)} 
+              />
+              <input 
+                type='text' 
+                placeholder='Password...'
+                value={password} 
+                onChange={(evt) => setPassword(evt.target.value)} 
+              />
+              <input 
+                type='text' 
+                placeholder='Password Confirm...'
+                value={passwordConfirm} 
+                onChange={(evt) => setPasswordConfirm(evt.target.value)} 
+              />
+              <input type="submit" value="Submit" />
+            </form>
+            { errorMessage === null ? null : <div>{errorMessage}</div>}
+            <div>
+              <Link to="/">Back to Login</Link>
+            </div>
+        </>) 
+        : <Redirect to="/" />
+      }
     </>
   )
 }
