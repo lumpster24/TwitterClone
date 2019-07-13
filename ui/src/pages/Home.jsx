3ff2aa8984e892ca92e0ced43691a3a883f385ad
Ian <ianbingham24@gmail.com>
2019-07-12 18:33:33 -0700

    get logged in user on Home page

--- ui/src/pages/Home.jsx
+++ ui/src/pages/Home.jsx
@@ -1,6 +1,24 @@
-import React from "reactn"
+import React, { useGlobal, useState, useEffect } from "reactn"
+import axios from 'axios'
 
 const Home = () => {
+  const [token, setToken] = useGlobal('token')
+  const [currentUser, setCurrentUser] = useState('')
+
+  const baseUrl = 'http://localhost:4000'
+
+  useEffect(() => {
+    const getAllUsers = async () => {
+      const { data } = await axios.get(`${baseUrl}/user/profile`, {
+        headers: { 'Authorization' : `Bearer ${token}` }
+      })
+
+      setCurrentUser(data)
+    }
+
+    getAllUsers()
+  }, [token])
+
   return (
     <div>
       Home page
--- ui/src/pages/Login.jsx
+++ ui/src/pages/Login.jsx
@@ -12,7 +12,6 @@ const Login = () => {
 
   const onSubmit = async (evt) => {
     evt.preventDefault()
-    setLoginSuccess(false)
 
     try {
       const { data } = await axios.post(`${baseUrl}/auth/login`, {
